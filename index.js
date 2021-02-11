4897c6a131b6c55e20c1b4496e8112d2c704679f
Sean <skabe89@gmail.com>
2021-02-11 13:00:49 -0500

    commit tree d9abeead17b1943b4c54e4035e00d218e8fec63f

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+function writeCards(names){
+  let thankYou = []
+  for(let i = 0; i < names.length; i += 1){
+    thankYou.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+  }
+  return thankYou
+}
+
+
+function countDown(number){
+  while (number >= 0){
+    console.log(number)
+    number --
+  }
+}
